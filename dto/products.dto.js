417e38739457afd8e3ceecb950e11cef7deaaa30
Konstantin Rodin <k.rodin@geniusee.com>
2021-11-29 08:50:20 +0200

    Add Title to DTO

--- dto/products.dto.js
+++ dto/products.dto.js
@@ -1,8 +1,9 @@
 export class ProductsDto {
-  constructor({ id, image, category, description, price }) {
+  constructor({ id, image, category, title, description, price }) {
     this.id = id;
     this.image = image;
     this.category = category;
+    this.title = title;
     this.description = description;
     this.price = price;
   }
